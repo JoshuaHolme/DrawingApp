e70a2393e819e8c14878bb9dee085af5bdb430de
Joshua Holme <joshuaholme19@icloud.com>
2020-04-22 09:26:10 -0400

    Created General App Layout

---
+++ Drawing App/View/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Drawing App
+//
+//  Created by Joshua Holme on 4/22/20.
+//  Copyright © 2020 Joshua Holme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ZStack {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 100)
+                Text("Control\nArea")
+                    .foregroundColor(Color.white)
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color.black)
+                Text("Artboard")
+                    .foregroundColor(Color.white)
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 300)
+                Text("Layer Browser")
+                    .foregroundColor(Color.white)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
